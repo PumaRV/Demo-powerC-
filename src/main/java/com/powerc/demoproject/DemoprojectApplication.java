54b42147b61bc05ac961c2a0de87d6d37d984e33
Ottoniel Rodriguez <ottoniel.rodriguez@elinvar.de>
2022-03-01 22:38:14 +0100

    Initial commit

---
+++ src/main/java/com/powerc/demoproject/DemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.powerc.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojectApplication.class, args);
+	}
+
+}
